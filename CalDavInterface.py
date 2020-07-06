d89a9da5d5d82c4974c427a812ca36786bbf9778
Jannik Schlemmer <js329@hdm-stuttgart.de>
2020-07-06 17:45:53 +0200

    added functionality for next appointment request

--- CalDavInterface.py
+++ CalDavInterface.py
@@ -1,8 +1,10 @@
 import caldav
 import json
 from datetime import datetime
-from icalendar import Calendar, Event
+import icalendar
+import pytz
 
+utc = pytz.UTC
 
 def delete_events(events):
     for event in events:
@@ -12,15 +14,17 @@ def delete_events(events):
 def parse_ics_events(events):
     parsed_events = []
     for event in events:
-        cal = Calendar.from_ical(event.data, True)
+        cal = icalendar.Calendar.from_ical(event.data, True)
         [parsed_events.append(get_event_details(e)) for e in cal[0].walk("vevent")]
     return parsed_events
+
 
 def get_event_details(event):
     start = event["DTSTART"].dt
     end = event["DTEND"].dt
     title = str(event["SUMMARY"])
     return {"title": title, "starttime": start, "endtime": end}
+
 
 class CalDavInterface:
     def __init__(self):
@@ -37,7 +41,17 @@ class CalDavInterface:
         my_principal = self.client.principal()
         return my_principal.calendars()[0]
 
-    def get_events_for_timeperiod(self, calendar, startdate, enddate):
-        events = calendar.date_search(start=startdate, end=enddate, expand=True)
+    def get_events_for_timeperiod(self, startdate, enddate):
+        events = self.calendar.date_search(start=startdate, end=enddate, expand=True)
         return parse_ics_events(events)
 
+    def get_next_event(self):
+        all_events = self.calendar.events()
+        parsed_events = parse_ics_events(all_events)
+        sorted_events = sorted(parsed_events, key=lambda i: i["starttime"])
+        for event in sorted_events:
+            starttime = event["starttime"]
+            now = datetime.today()
+            current_time = now.replace(tzinfo=starttime.tzinfo)
+            if starttime > current_time:
+                return event
--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 from mycroft import MycroftSkill, intent_file_handler
+from .CalDavInterface import *
+import calendar
 
-class NexcloudCalendar(MycroftSkill):
+class NextcloudCalendar(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.caldav_interface = CalDavInterface()
 
     @intent_file_handler('calendar.nexcloud.intent')
     def handle_calendar_nexcloud(self, message):
@@ -16,6 +19,30 @@ class NexcloudCalendar(MycroftSkill):
         # TODO: Delete all appointments and respond with confirmation on cancellation OR
         # TODO: respond with summary of appointments that will be deleted and ask for confirmation
 
+    @intent_file_handler('get.appointment.date.intent')
+    def handle_appointment_request_for_datetime(self, message):
+        date = message.data.get('date')  # TODO: parse date
+        time = message.data.get('time') # TODO: parse time
+        # TODO: implement interface logic
+
+    @intent_file_handler('get.next.appointment.intent')
+    def handle_get_next_appointment(self, message):
+        next_event = self.caldav_interface.get_next_event()
+        title = next_event["title"]
+        startdate_time = next_event["starttime"]
+        date_formatted = "{month} {day}, {year}".format(
+            month=calendar.month_name[startdate_time.month],
+            day=startdate_time.day,
+            year=startdate_time.year
+        )
+        time_formatted = startdate_time.strftime("%I:%M %p")
+        data = {
+            "date": date_formatted,
+            "time": time_formatted,
+            "title": title
+        }
+        self.speak_dialog('next.appointment.details.dialog', data)
+
 
 def create_skill():
-    return NexcloudCalendar()
+    return NextcloudCalendar()
